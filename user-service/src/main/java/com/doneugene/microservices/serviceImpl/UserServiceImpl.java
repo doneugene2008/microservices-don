876f8286e21887c8405f5811f5e265e5f4d5c872
Eugene Awonu Akyea <eugeneak@stlghana.com>
2022-02-17 16:57:08 +0000

    Refactoring to remove junk code

--- user-service/src/main/java/com/doneugene/microservices/serviceImpl/UserServiceImpl.java
+++ user-service/src/main/java/com/doneugene/microservices/serviceImpl/UserServiceImpl.java
@@ -2,17 +2,14 @@ package com.doneugene.microservices.serviceImpl;
 
 
 import com.doneugene.microservices.dto.DepartmentDTO;
-import com.doneugene.microservices.dto.ServiceAvailableDTO;
 import com.doneugene.microservices.dto.UserResponseDTO;
 import com.doneugene.microservices.model.User;
 import com.doneugene.microservices.repository.UserRepository;
 import com.doneugene.microservices.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,9 +22,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final RestTemplate restTemplate;
-    private static final String ghanaGovURL = "https://www.govgh.org/api/v1.1/checkout/";
-    private static final String ghanaGovURLPrivateKey = "f5777e6d979478f5ab42c5536aaad1804ef7740f4bb6a72657ae41c027cfb6e2809b20cb5874581b34f44ee8e40bbcd59f2a901fab84ba2306cf52f0998196aec6b3954720dce4d5fafa96e0ca92dc8af6b5b9281eb9b6a49b7fc6d8ed98fd8da6d9180b7c7368db45b4c767fdedf820";
-    private static final Integer ghanaGovURLCollection = 210100;
 
 
     @Override
@@ -61,7 +55,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponseDTO fetchUserWithDepartment(UUID userId) {
         var userResponseDTO = new UserResponseDTO();
-        /*try {*/
             /*  Fetch User FROM User Service  */
             User user = this.fetchUserById(userId);
 
@@ -73,32 +66,7 @@ public class UserServiceImpl implements UserService {
             userResponseDTO.setUser(user);
             userResponseDTO.setDepartment(department);
 
-
-            /*  Make Call To Doc Service To get Document    */
-            /*UriComponentsBuilder builder = UriComponentsBuilder
-                    .fromUriString(ghanaGovURL.concat("service.php"))
-                    .queryParam("request", "get")
-                    .queryParam("api_key", ghanaGovURLPrivateKey)
-                    .queryParam("collection_agent_branch_code", ghanaGovURLCollection)
-                    .queryParam("search_field", "all")
-                    .queryParam("current_page", 0)
-                    .queryParam("results_per_page", 1000)
-                    .queryParam("sort_by", "name")
-                    .queryParam("sort_ascending", true);
-
-            System.out.println("builder.buildAndExpand().toString() = " + builder.build().toUriString());
-
-            ServiceAvailableDTO ghGovApiResponse = restTemplate
-                    .getForObject(builder.build().toUriString(), ServiceAvailableDTO.class);
-
-            System.out.println("ghGovApiResponse = " + ghGovApiResponse);*/
-
             return userResponseDTO;
 
-        /*}catch (Exception e){
-
-            log.error("Error while fetching user with department, Cause -> {}, Message -> {}", e.getCause(), e.getMessage());
-        }
-        return null;*/
     }
 }
